fab6d1b3b802c0bc469192a8f282a1af0764b25c
Sergey Home <true.kapitan@gmail.com>
2024-07-09 00:41:34 +0700

    Определение типа входящих файлов

--- mailboxes/catch_email.py
+++ mailboxes/catch_email.py
@@ -1,6 +1,7 @@
 from aiogram import Bot
-from aiogram.types import BufferedInputFile
+from aiogram.types import BufferedInputFile, InputMediaDocument
 from database import requests as rq
+from mailboxes.media import files_to_media
 from mailboxes.mail import MailFile, MailText
 import asyncio
 from dotenv import load_dotenv
@@ -13,7 +14,6 @@ async def send_emails() -> None:
     while True:
         async with Bot(token=os.getenv('TOKEN')) as bot:
             rules = await rq.get_all_rules()
-            print(len(rules))
             for rule in rules:
                 if rule[1] == 'all':
                     pass
@@ -21,9 +21,8 @@ async def send_emails() -> None:
                     mail = MailFile(email=rule[2], password=rule[3], from_email=rule[0])
                     files = mail.get_response()
                     if files:
-                        for name, file in files:
-                            await bot.send_document(chat_id=rule[4],
-                                                    document=BufferedInputFile(file=file, filename=name), caption=name)
+                        media = files_to_media(files)
+                        await bot.send_media_group(chat_id=rule[4], media=media)
                 elif rule[1] == 'text':
                     mail = MailText(email=rule[2], password=rule[3], from_email=rule[0])
                     text = mail.get_response()
---
+++ mailboxes/media.py
@@ -0,0 +1,42 @@
+from aiogram.types import InputMediaDocument, InputMediaPhoto, InputMediaAudio, InputMediaVideo, BufferedInputFile
+
+
+def detect_file_type(filename: str) -> str:
+    if filename.split('.')[1] in ('aif', 'aiff', 'wav', 'mid', 'ogg', 'flac', 'mp3',
+                                  'mp4', 'aac', 'm4a', 'aifc', 'rm', 'ra', 'wma'):
+        return 'Audio'
+    if filename.split('.')[1] in ('sxi', 'odg', 'svg', 'vsd', 'eps', 'cwk', 'wp', 'ott', 'asp', 'cdd',
+                                  'cpp', 'dotm', 'gpx', 'indd', 'kdc', '.kml', 'mdb', 'mdf', 'mso', 'one',
+                                  'pkg', 'pl', 'pot', 'potm', 'potx', 'ppsm', 'ps', 'sdf', 'sgml', 'sldm',
+                                  'xar', 'xlt', 'xltm', 'xltx', 'pdf', 'txt', 'doc', 'odt', 'xps', 'chm',
+                                  'rtf', 'sxw', 'docx', 'wpd', 'wps', 'docm', "hwp", 'pub', 'xml', 'log',
+                                  'oxps', 'vnt', 'dot', 'pages', 'm3u', 'dotx', 'shs', 'msg', 'odm', 'pmd',
+                                  'vmg', 'eml', 'tex', 'wp5', 'csk', 'fdxt', 'adoc', 'afpub', 'tcr', 'acsm',
+                                  'opf', 'mbp', 'apnx', 'cbt', 'vbk', 'kfx', 'lrf', 'snb', 'odp', 'ppt',
+                                  'pptx', 'pps', 'ppsx', 'pptm', 'key', "flipchart", 'epub', 'mobi', 'azw',
+                                  'azw3', 'fb2', 'djvu', 'cbz', 'cbr', 'ibooks', 'lit', 'pdb', 'prc', 'tr2',
+                                  'tr3', 'ods', 'xls', 'xlsx', 'csv', 'wks', 'xlsm', 'xlsb', 'xlr', 'wk3',
+                                  'numbers'):
+        return 'Document'
+    if filename.split('.')[1] in ('jpg', 'jpeg', 'png', 'gif', 'raw', 'svg', 'bmp', 'ico', 'tiff', 'webp'):
+        return 'Photo'
+    if filename.split('.',)[1] in ('3g2','3gp','3gp2','3gpp','3gpp2','asf','asx','avi','bin','dat','drv',
+                                  'f4v','flv','gtp','h264','m4v','mkv','mod','moov','mov','mp4','mpeg','mpg',
+                                  'mts','rm','rmvb','spl','srt','stl','swf','ts','vcd','vid','vid','vid',
+                                  'vob','webm','wm','wmv','yuv'):
+        return 'Video'
+
+
+def files_to_media(files: list[tuple[str, bytes]]) -> list[InputMediaAudio | InputMediaDocument | InputMediaPhoto |
+                                                           InputMediaVideo]:
+    media = []
+    for name, file in files:
+        if detect_file_type(name) == 'Document':
+            media.append(InputMediaDocument(media=BufferedInputFile(file=file, filename=name)))
+        elif detect_file_type(name) == 'Audio':
+            media.append(InputMediaAudio(media=BufferedInputFile(file=file, filename=name)))
+        elif detect_file_type(name) == 'Photo':
+            media.append(InputMediaPhoto(media=BufferedInputFile(file=file, filename=name)))
+        elif detect_file_type(name) == 'Video':
+            media.append(InputMediaVideo(media=BufferedInputFile(file=file, filename=name)))
+    return media
